a5b1adda115ac7a2f6bfd0e122f6fca1751ccf4e
Alexey Scripkin <a.scripkin@gmail.com>
2017-09-19 11:06:18 +0300

    party member type

--- src/RPParty.hs
+++ src/RPParty.hs
@@ -1,2 +1,35 @@
 module RPParty where
 
+import           Dices
+
+data AttribTemplate = ATml {
+    throw :: Throw,
+    start :: Int,
+    top   :: Int
+  }
+
+data MemberTemplate = MTml {
+    healthTml :: AttribTemplate,
+    armorTml  :: AttribTemplate,
+    weaponTml :: AttribTemplate
+  }
+
+data Member = Member {
+    health :: Int,
+    armor  :: Int,
+    weapon :: Int
+  }
+
+type Party = [Member]
+
+genAttrFromTemplate :: AttribTemplate -> IO Int
+genAttrFromTemplate atml = do
+    dc <- cast (throw atml) (top atml)
+    return (sumd dc)
+
+genMemberFromTemplate :: MemberTemplate -> IO Member
+genMemberFromTemplate tml = do
+    h <- genAttrFromTemplate (healthTml tml)
+    a <- genAttrFromTemplate (armorTml tml)
+    w <- genAttrFromTemplate (weaponTml tml)
+    return Member { health = h, armor = a, weapon = w }
